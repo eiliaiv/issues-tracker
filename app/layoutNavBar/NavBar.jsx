dcd0ffaedb38919ba7fe83448c31936d9a13de7a
eilia <eiliababatabar@gmail.com>
2026-06-13 04:20:52 -0500

    make nav bar UI better && and add tp nav bar and home page session

--- app/layoutNavBar/NavBar.jsx
+++ app/layoutNavBar/NavBar.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 import { FaBug } from "react-icons/fa";
 import { usePathname } from 'next/navigation';
 import classname from 'classnames';
-import { Box } from '@radix-ui/themes';
-import { useSession } from 'next-auth/react';
+import { Avatar, Box, Text, DropdownMenu } from '@radix-ui/themes';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const NavBar = () => {
   const pathname = usePathname();
@@ -14,25 +14,37 @@ const NavBar = () => {
     { label: "issues", href: "/issues" },
   ]
   return (
-    <nav className="flex space-x-6 border-b mb-5 h-14 px-5 items-center">
-      <Link href="/"><FaBug className="hover:scale-120 transition-transform" /></Link>
-      <ul className='flex space-x-6'>
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link className={classname({
-              "text-zinc-900": pathname === link.href,
-              "text-zinc-500": pathname !== link.href,
-              "hover:text-zinc-800 transition-colors": true
-            })} href={link.href}>
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-          {status === "authenticated" && <Link href="/api/auth/signout">Log out</Link>}
-          {status === "unauthenticated" && <Link href="/api/auth/signin">Log in</Link>}
-      </Box>
+    <nav className=" border-b mb-5 py-3 px-10">
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center space-x-5'>
+          <Link href="/"><FaBug className="hover:scale-120 transition-transform" /></Link>
+          <ul className='flex space-x-6'>
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link className={classname({
+                  "text-zinc-900": pathname === link.href,
+                  "text-zinc-500": pathname !== link.href,
+                  "hover:text-zinc-800 transition-colors": true
+                })} href={link.href}>
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <Avatar src={session?.user?.image || undefined} fallback={session?.user?.name?.[0]?.toUpperCase()} variant='solid' radius='full'></Avatar>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+              <DropdownMenu.Label><Text className='text-black' size="4">{session?.user?.email}</Text></DropdownMenu.Label>
+              <DropdownMenu.Item><Box className="text-white bg-blue-700 w-full text-center rounded h-10 font-semibold text-lg pt-1.25">{status === "authenticated" && <Link href="/api/auth/signout">Log out</Link>}
+                {status === "unauthenticated" && <Link href="/api/auth/signin">Log in</Link>}</Box></DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        </div>
+      </div>
     </nav >
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
+"use client"
+
+import { useSession } from "next-auth/react";
 
 
 export default function Home() {
+  const { status, data: session } = useSession()
   return (
     <>
-      <div>hello</div>
+      <div className="font-bold text-3xl">Hello, {session?.user?.name} </div>
     </>
 
   );
